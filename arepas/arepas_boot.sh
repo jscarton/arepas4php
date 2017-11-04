9b3b382bde8ce81554641d10a97b3aa4109c1e5f
jscarton <jscarton@gmail.com>
2017-11-04 10:20:37 -0500

    first version of scripts

---
+++ arepas/arepas_boot.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#echo "---------------------------------------------------------------"
+#echo "			RUNNING AREPAS4PHP CUSTOM BOOT SCRIPT"
+#echo "---------------------------------------------------------------"
+echo "Configuring iptables"
+iptables -F
---
+++ arepas/arepas_installer.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+echo "---------------------------------------------------------------"
+echo " "
+echo " "
+echo "      AREPAS4PHP INSTALLER v20171015"
+echo " "
+echo " "
+echo "---------------------------------------------------------------"
+yum -y install epel-release
+yum update
+yum -y install nano wget
+
+
+echo "---------------------------------------------------------------"
+echo "INSTALLING GIT"
+echo "---------------------------------------------------------------"
+yum -y install git
+
+echo "---------------------------------------------------------------"
+echo "INSTALLING NGINX"
+echo "---------------------------------------------------------------"
+yum install nginx
+systemctl start nginx
+systemctl enable nginx
+
+echo "---------------------------------------------------------------"
+echo "INSTALLING PHP 7.1"
+echo "---------------------------------------------------------------"
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+yum install -y php71w-cli php71w-common php71w-gd php71w-mbstring php71w-mcrypt php71w-mysqlnd php71w-xml php71w-fpm
+systemctl start php-fpm
+systemctl enable php-fpm
+
+echo "---------------------------------------------------------------"
+echo " AREPAS4PHP SETUP HAS BEEN COMPLETED"
+echo "---------------------------------------------------------------"
