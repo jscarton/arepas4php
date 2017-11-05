d632477a004fe7398d6fbbf3e5c080a753660d0e
jscarton <jscarton@gmail.com>
2017-11-05 12:44:33 -0500

    fix nginx site setup

--- arepas/recipes/nginx/php/install.sh
+++ arepas/recipes/nginx/php/install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 cp setup/php-fpm.conf /etc/nginx/conf.d/
-cp setup/php.arepasapp.conf /etc/nginx/conf.d/
+cp setup/php.arepasapp.com.conf /etc/nginx/conf.d/
 cp setup/php.ini /etc/
 cp setup/www.conf /etc/php-fpm.d/
 mkdir -p /etc/jscarton/ssl
 cp ssl/* /etc/jscarton/ssl/
 setenforce 0
+service nginx restart
+service php-fpm restart
