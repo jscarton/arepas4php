096e2551a350023086f798075b3b7510e3913e0a
jscarton <jscarton@gmail.com>
2018-01-25 15:13:51 -0500

    trying to fix minor issues installing dependencies

--- arepas/arepas_installer.sh
+++ arepas/arepas_installer.sh
@@ -38,8 +38,8 @@ echo "---------------------------------------------------------------"
 rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 rpm -ivh mysql-community-release-el7-5.noarch.rpm
 yum update
-yum install mysql-server
-systemctl start mysqldp
+yum install -y mysql-server
+systemctl start mysqld
 systemctl enable mysqld
 
 echo "---------------------------------------------------------------"
@@ -47,8 +47,8 @@ echo "INSTALLING PHPunit"
 echo "---------------------------------------------------------------"
 wget https://phar.phpunit.de/phpunit.phar
 chmod +x phpunit.phar
-mv phpunit.phar /usr/local/bin/phpunit
-phpunit --version
+#mv phpunit.phar /usr/local/bin/phpunit
+#phpunit --version
 
 echo "---------------------------------------------------------------"
 echo "INSTALLING LATEST COMPOSER"
@@ -57,15 +57,15 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-mv composer.phar /usr/local/bin/composer
-composer --version
+#mv composer.phar /usr/local/bin/composer
+#composer --version
 
 echo "---------------------------------------------------------------"
 echo "INSTALLING LATEST Laravel"
 echo "---------------------------------------------------------------"
-composer global require "laravel/installer"
-echo "export PATH=\$PATH:\$HOME/.config/composer/vendor/bin/">>/home/vagrant/.bash_profile
-laravel --version
+#composer global require "laravel/installer"
+#echo "export PATH=\$PATH:\$HOME/.config/composer/vendor/bin/">>/home/vagrant/.bash_profile
+#laravel --version
 
 echo "---------------------------------------------------------------"
 echo "CONFIGURING FIRST SITE"
