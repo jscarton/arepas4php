fba840c8c0658719c3b1f9e15a7d1642ecf618a4
jscarton <jscarton@gmail.com>
2018-01-25 13:12:48 -0500

    remove sudo word from commands

--- arepas/arepas_installer.sh
+++ arepas/arepas_installer.sh
@@ -35,19 +35,19 @@ systemctl enable php-fpm
 echo "---------------------------------------------------------------"
 echo "INSTALLING MYSQL 5.6"
 echo "---------------------------------------------------------------"
-sudo rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
-sudo yum update
-sudo yum install mysql-server
-sudo systemctl start mysqldp
-sudo systemctl enable mysqld
+rpm -Uvh http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum update
+yum install mysql-server
+systemctl start mysqldp
+systemctl enable mysqld
 
 echo "---------------------------------------------------------------"
 echo "INSTALLING PHPunit"
 echo "---------------------------------------------------------------"
 wget https://phar.phpunit.de/phpunit.phar
 chmod +x phpunit.phar
-sudo mv phpunit.phar /usr/local/bin/phpunit
+mv phpunit.phar /usr/local/bin/phpunit
 phpunit --version
 
 echo "---------------------------------------------------------------"
@@ -57,7 +57,7 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer
 composer --version
 
 echo "---------------------------------------------------------------"
